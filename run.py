8d81504e3d6dbf0503f940d74ae33d92db04c1d2
dorkitude <dork@dorkitude.com>
2010-10-26 17:17:07 -0700

    added PHP mode

--- run.py
+++ run.py
@@ -6,23 +6,68 @@ Created by dorkitude on 2010-10-25.
 """
 
 import os
-import python_memcached_fork.memcache as memcache
+from python_memcached_fork import memcache
+import binascii # used for php_mode
+import pickle
+from PHPSerialize_Scott_Hurring.PHPSerialize import PHPSerialize
+from PHPSerialize_Scott_Hurring.PHPUnserialize import PHPUnserialize
 
 # this line forces python into adorable mode (referred to by some as 'interactive mode')
 os.environ['PYTHONINSPECT'] = '1'
 
 SETTINGS = {
-    'hosts' : [ '127.0.0.1:11211' ]
+    'hosts' : [ 'localhost:11211' ], # a list of servers:port strings
+    'php_mode' : False, # if true, adorable will hash keys and use php-style serialization/deserialization on values.
 }
 
+
+
+
+
+
+# if we're in php_mode, make sure the keys get hashed in php style    
+if SETTINGS['php_mode']:
+    print ""
+    print ""
+    print ""
+    print "php mode: TRUE"
+    print ""
+    print ""
+    print ""
+    def php_hash(key):
+        return (binascii.crc32(key) >> 16) & 0x7fff # borrowed from http://groups.google.com/group/memcached/msg/7bb75a026c44ec43
+    memcache.serverHashFunction = php_hash
     
+
+class AdorableClient(memcache.Client):
+    """docstring for AdorableMemcache"""
+    def __init__(self, settings):
+
+        if settings['php_mode']:
+            pickler = AdorablePicklerPHP
+            unpickler = AdorableUnpicklerPHP
+        else:
+            pickler = pickle.Pickler
+            unpickler = pickle.Unpickler
+        
+        super(AdorableClient, self).__init__(servers=settings['hosts'], pickler=pickler, unpickler=unpickler)
+        
+
+class AdorablePicklerPHP(pickle.Pickler):
+    """docstring for AdorablePicklerPHP"""
+    dump = PHPSerialize().serialize
+    
+class AdorableUnpicklerPHP(pickle.Unpickler):
+    """docstring for AdorableUnpicklerPHP"""
+    load = PHPUnserialize().unserialize
+
 def show_startup_text():
     f = open('STARTUP')
     print "".join(f.readlines())
     f.close() 
 
-def get_client():
-    return memcache.Client(SETTINGS['hosts'], debug=0)
+def get_client(settings):
+    return AdorableClient(settings)
 
 def help():
     f = open('USAGE')
@@ -32,6 +77,4 @@ def help():
     
 if __name__ == "__main__":
     show_startup_text()
-    ac = get_client()
-    
-    
+    ac = get_client(SETTINGS)
